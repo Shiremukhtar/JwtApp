0ce0e7bc12029e80e3dc6114a48a10f1c150d329
Mukhtar Shire <cuncu@DESKTOP-PQ5FM4I>
2023-03-17 02:24:26 +0100

    first commit

---
+++ JwtApp/Models/UserConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace JwtApp.Models
+{
+    public class UserConstants
+    {
+        public static List<UserModel> Users = new List<UserModel>() {
+            new UserModel(){Username = "jason_admin", Email = "jason_admin@email.com", Password =
+                "Mypass123", GivenName = "Jason", Surname = "Adan", Role = "Adminstrator"},
+            new UserModel(){Username = "rage_seller", Email = "rage_seller@mail.com", Password =
+                "MyPass_w0rd", GivenName = "Rage", Surname="Diriye", Role = "Seller"},
+        };
+    }
+}
